1f40aaf5d0306d903991defa449d20721882d10d
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2022-02-25 20:26:58 -0600

    Allow Tampermonkey script on raysweather.com
    Closes #1

--- BooneWeatherFix.user.js
+++ BooneWeatherFix.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @match      http*://www.booneweather.com/*
+// @match      http*://www.raysweather.com/*
 // @name       BooneWeatherFix
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
